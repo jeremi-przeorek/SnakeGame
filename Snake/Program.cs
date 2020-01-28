081d2145fd0bd189625da0f525b3392d18b51b83
jeremi-przeorek <57287376+jeremi-przeorek@users.noreply.github.com>
2020-01-28 16:35:08 +0100

    Add key handling (left,right,up,down)

---
+++ Snake/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            //game loop
+            while(!exit)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo input = Console.ReadKey();
+                }
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
