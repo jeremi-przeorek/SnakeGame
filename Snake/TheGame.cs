b3be04ce102343adb6a7c4d506e2a02dcb4f93f8
jeremi-przeorek <57287376+jeremi-przeorek@users.noreply.github.com>
2020-01-29 15:49:45 +0100

    Add new class TheGame

---
+++ Snake/TheGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class TheGame
+    {
+        public TheGame (int numberOfPlayers, int numberOfSnacks)
+        {
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                Snakes.Add(new Snake(5 * i, 1));
+            }
+            for (int i = 0; i < numberOfSnacks; i++)
+            {
+                Snacks.Add(new Snack());
+            }
+        }
+        public List<Snake> Snakes { get; set; }
+        public List<Snack> Snacks { get; set; }
+    }
+}
