9e9a12ce123478cd9f1ff285848c117ff498a72e
jeremi-przeorek <57287376+jeremi-przeorek@users.noreply.github.com>
2020-01-29 10:54:54 +0100

    Add IsInSnake method, fix Draw() & Erase() for testing
    Method checks if the snack is in snacke
    Also added if(!Co nsole.IsOutputRedirected) to avoid throwing exception
    "The handle is invalid" while testing to Draw() and Erase() methods.

--- Snake/Snack.cs
+++ Snake/Snack.cs
@@ -16,13 +16,39 @@ namespace Snake
             CurrentTarget = new Coordinate(x, y);
             Draw();
         }
+        public Snack(int x, int y)
+        {
+            CurrentTarget = new Coordinate(x, y);
+            Draw();
+        }
         public Coordinate CurrentTarget { get; set; }
 
         public void Draw()
         {
-            Console.SetCursorPosition(CurrentTarget.X, CurrentTarget.Y);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("$");
+            if (!Console.IsOutputRedirected)
+            {
+                Console.SetCursorPosition(CurrentTarget.X, CurrentTarget.Y);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("$");
+            }
+        }
+
+        public void Erase()
+        {
+            if (!Console.IsOutputRedirected)
+            {
+                Console.SetCursorPosition(CurrentTarget.X, CurrentTarget.Y);
+                Console.WriteLine(" ");
+            }
+        }
+
+        public bool IsInSnake(Snake snake)
+        {
+            if(snake.Tail.Where( c => c.Equals(this.CurrentTarget)).ToList().Count > 0 || snake.HeadCoordinate.Equals(this.CurrentTarget))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
