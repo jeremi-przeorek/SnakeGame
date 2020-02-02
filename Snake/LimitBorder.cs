7f7fd9a59ebfa7adc4b033afcaa0323ef901511d
jeremi-przeorek <57287376+jeremi-przeorek@users.noreply.github.com>
2020-02-02 17:19:03 +0100

    new class LimitBorder

---
+++ Snake/LimitBorder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class LimitBorder : Rectangle
+    {
+        public LimitBorder(Coordinate leftUpCorner, Coordinate rightDownCorner, char symbol) : base (leftUpCorner,rightDownCorner,symbol) 
+        {
+            CreateEveryElementList();
+        }
+
+        List<Coordinate> EveryElementOfBorder = new List<Coordinate>();
+
+        public void CreateEveryElementList()
+        {
+            int numberOfWalls = 4;
+            for (int i = 0; i < numberOfWalls; i++)
+            {
+                for (int j = 0; j < Walls[i].ElementsWall.Count-1; j++)
+                {
+                    EveryElementOfBorder.Add(Walls[i].ElementsWall[j].coordinate);
+                }
+            }
+        }
+    }
+}
