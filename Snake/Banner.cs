77502cdb0a5af10b0f68b83017c9227535dba60a
jeremi-przeorek <57287376+jeremi-przeorek@users.noreply.github.com>
2020-02-04 19:54:46 +0100

    Add Banner class with logo

---
+++ Snake/Banner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Banner
+    {
+        public Banner(Coordinate position)
+        {
+            Position = position;
+            Paint();
+        }
+        readonly string[] Logo = new string[] {
+            @".-- -.         .       ,---.               ",
+            @" \___  ,-. ,-. | , ,-. |  -'  ,-. ,-,-. ,-.",
+            @"     \ | | ,-| |<  |-' |  ,-' ,-| | | | |-'",
+            @" `---' ' ' `-^ ' ` `-' `---|  `-^ ' ' ' `-'",
+            @"                        ,-.|               ",
+            @"                        `-+'               "
+        };
+
+        ConsoleColor Color = ConsoleColor.Red;
+        Coordinate Position = new Coordinate(1, 1);
+
+        public void Paint()
+        {
+            Console.ForegroundColor = Color;
+            for (int i = 0; i < Logo.Length; i++)
+            {
+                Console.SetCursorPosition(Position.X, Position.Y + i);
+                Console.Write(Logo[i]);
+            }
+        }
+    }
+}
