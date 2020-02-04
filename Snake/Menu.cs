5b1e23714bb10295880c0c179489365a034b874b
jeremi-przeorek <57287376+jeremi-przeorek@users.noreply.github.com>
2020-02-04 19:55:13 +0100

    Develop Menus handling

--- Snake/Menu.cs
+++ Snake/Menu.cs
@@ -12,21 +12,24 @@ namespace Snake
         {
             bool exitMenu = false;
             Console.Clear();
-            Slot play = new Slot("Play", new Coordinate(10, 10),"PlayGame");
-            Slot highScore = new Slot("High Scores", new Coordinate(10, 12),"OpenHighScores");
-            Slot exit = new Slot("Exit", new Coordinate(10, 14), "ExitGame");
+            Banner banner = new Banner(new Coordinate(9, 5));
+            Coordinate SlotsPosition = new Coordinate(25, 13);
+            Slot play = new Slot("Play", SlotsPosition, "PlayGame");
+            Slot highScore = new Slot("High Scores", SlotsPosition + new Coordinate(0, 2), "OpenHighScores");
+            Slot exit = new Slot("Exit", SlotsPosition + new Coordinate(0, 4), "Exit");
             Slot[] slots = { play, highScore, exit };
-            Block indicator = new Block(8, 10, '>');
+            Rectangle border = new Rectangle(new Coordinate(1, 1), new Coordinate(59, 29), '#');
+            Indicator indicator = new Indicator(SlotsPosition + new Coordinate(-2, 0), '>');
 
             string output = "";
-            
+
             while (!exitMenu)
             {
                 ConsoleKeyInfo input = Console.ReadKey(true);
                 switch (input.Key)
                 {
                     case ConsoleKey.DownArrow:
-                        indicator.Move(0,2);
+                        indicator.Move(0, 2);
                         break;
                     case ConsoleKey.UpArrow:
                         indicator.Move(0, -2);
@@ -37,11 +40,47 @@ namespace Snake
                         exitMenu = true;
                         break;
                 }
-                
+
             }
             return output;
         }
-        static Coordinate Position;
-        static char SelectSymbol = '>';
+        public static string ShowAfterDeath()
+        {
+            bool exitMenu = false;
+            Console.Clear();
+            //Banner banner = new Banner(new Coordinate(9, 5));
+            Coordinate SlotsPosition = new Coordinate(25, 13);
+            Slot play = new Slot("Play again", SlotsPosition, "PlayGame");
+            Slot highScore = new Slot("Main Menu", SlotsPosition + new Coordinate(0, 2), "Main Menu");
+            Slot exit = new Slot("Exit", SlotsPosition + new Coordinate(0, 4), "Exit");
+            Slot[] slots = { play, highScore, exit };
+            Rectangle border = new Rectangle(new Coordinate(1, 1), new Coordinate(59, 29), '#');
+            Indicator indicator = new Indicator(SlotsPosition + new Coordinate(-2, 0), '>');
+
+            string output = "";
+
+            while (!exitMenu)
+            {
+                int actualIndicatorPosition = slots.Length;
+                ConsoleKeyInfo input = Console.ReadKey(true);
+                switch (input.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        indicator.Move(0, 2);
+                        actualIndicatorPosition--;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        indicator.Move(0, -2);
+                        actualIndicatorPosition--;
+                        break;
+                    case ConsoleKey.Enter:
+                        var selectedMenu = slots.Where(x => x.Position.Y.Equals(indicator.Position.Y)).ToArray();
+                        output = selectedMenu[0].OpensMenu;
+                        exitMenu = true;
+                        break;
+                }
+            }
+            return output;
+        }
     }
 }
